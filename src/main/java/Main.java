599d05f206a26b6a1fd42c6beb43fa23b46e9688
Oleksandr Fomin <105858589+Qilibrium@users.noreply.github.com>
2022-11-07 12:24:34 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,20 +6,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        //CREATE name
-       // new ClientService().create("new name");
-
-        //RETURN id, name
-       // new Client.ClientService().getById(7);
-
-        //UPDATE name
-       // new Client.ClientService().setName(1,"Demetrius Andrade");
-
-        //DELETE id
-       // new Client.ClientService().deleteById(7);
-
-
-        new Client.ClientService().listAll();
+      
 
 
     }
